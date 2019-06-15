f0c0629eb7d425f8c54e6dde6daab1baf1f536d7
Goutam Mishra <gm_mishra@hotmail.com>
2019-06-16 04:02:31 +0530

    Added Dependency for RestTemplate

--- BookStore/src/main/java/com/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/com/bookstore/BookStoreApplication.java
@@ -2,6 +2,9 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -9,5 +12,11 @@ public class BookStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
+	
+	@Bean
+	RestOperations restOperations() {
+		return new RestTemplate();
+	}
+	
 
 }
